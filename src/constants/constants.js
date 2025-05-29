3acad437721ba4520ec6048668cd1ef4dfce7090
KottaiRaja <110802858+KottaiRaja@users.noreply.github.com>
2025-05-29 10:54:54 +0530

    Update constants file

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,5 +2,5 @@ export const constants = {
     LINKEDIN: "https://www.linkedin.com/in/kottai-raja-065036245/",
     GITHUBURL: "",
     GMAIL: "kottairaja1220@gmail.com",
-    RESUME: 'https://drive.google.com/file/d/1TXFWhQKqteD87uAZkI3M2f3KbTe0ku-E/view?usp=drive_link'
+    RESUME: 'https://drive.google.com/file/d/1qWJp4A8OKFLIepQyqxFL3Qz7Wm4Ed1zs/view?usp=drive_link'
 }
